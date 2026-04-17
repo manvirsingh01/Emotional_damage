0e47c15ab834e8bf3653235b20a7b9e288106a83
manvirsingh01 <manvirbalap498@gmail.com>
2026-04-17 16:27:33 +0530

    this is theupdate

--- Emotional_damage/cli.py
+++ Emotional_damage/cli.py
@@ -1,4 +1,7 @@
 import argparse
+import os
+import sys
+
 
 def hello(args):
     print("Hello Manvir 😎")
@@ -26,3 +29,21 @@ def main():
         args.func(args)
     else:
         parser.print_help()
+
+    p3 = subparsers.add_parser("update")
+    p3.set_defaults(func=update)
+
+def update(args):
+    print("[*] Updating tool from GitHub...")
+
+    # Go to project directory
+    project_path = os.path.dirname(os.path.dirname(__file__))
+    os.chdir(project_path)
+
+    # Pull latest code
+    os.system("git pull")
+
+    # Reinstall tool
+    os.system(f"{sys.executable} -m pip install -e .")
+
+    print("[+] Update complete! Restart your command.")
